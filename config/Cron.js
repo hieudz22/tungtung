570d2a0d7585d90a1ec1161d81012f6282d0435a
hieudz22 <hieumonster8@gmail.com>
2025-05-31 18:01:53 +0700

    Cron.js

--- config/Cron.js
+++ config/Cron.js
@@ -1,22 +1,8 @@
+const cron = require('node-cron');
 
-let CronJob = require('cron').CronJob;
-let CronHu          = require('../app/Cron/EventHu');
-let XSMB            = require('../app/Cron/XSMB/XSMB');
-let XSMB_trathuong  = require('../app/Cron/XSMB/XSMB_trathuong');
-
-module.exports = function() {
-	new CronJob('0 0 0 * * *', function() {
-		CronHu();
-	}, null, true, 'Asia/Ho_Chi_Minh');
-
-	new CronJob('0 * 18 * * *', function() {
-		XSMB();
-	}, null, true, 'Asia/Ho_Chi_Minh');
-
-	new CronJob('0 35 18 * * *', function() {
-		XSMB_trathuong();
-	}, null, true, 'Asia/Ho_Chi_Minh');
-}
-git add config/cron.js
-git commit -m "Add cron config file"
-git push
+module.exports = function () {
+  // Cron đơn giản chạy mỗi 10 giây (chỉ để test)
+  cron.schedule('*/10 * * * * *', () => {
+    console.log('Cron job đang chạy mỗi 10 giây...');
+  });
+};
