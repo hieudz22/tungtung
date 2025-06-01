058f2f1feea32337e12523a7cebf8f3bc98e3622
hieudz22 <hieumonster8@gmail.com>
2025-06-01 10:21:40 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
   url: process.env.MONGODB_URI || 'mongodb+srv://admin:mzAxvlQCAEExNDa5@ac-iljwirw.rr8w15n.mongodb.net/CLUB3333?retryWrites=true&w=majority',
   options: {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   }
 };
