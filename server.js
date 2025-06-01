4c521244915e61e0ac05a2ff2e54de2c1d17076d
hieudz22 <hieumonster8@gmail.com>
2025-06-01 08:57:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ require('./routerCMS')(app, redT);	//load routes CMS
 require('./routerSocket')(app, redT); // load các routes WebSocket
 require('./app/Cron/taixiu')(redT);   // Chạy game Tài Xỉu
 require('./app/Cron/baucua')(redT);   // Chạy game Bầu Cua
-require('./config/cron')();
+//require('./config/cron')();
 require('./app/Telegram/Telegram')(redT); // Telegram Bot
-//require('./config/crontextchatdata')(); // copy text chat
-//require('./config/croncreateboot')();// create boot name duoc doc tu file
+require('./config/crontextchatdata')(); // copy text chat
+require('./config/croncreateboot')();// create boot name duoc doc tu file
 require('./config/cronchattx')(redT);// boot chat tài xiu
 app.listen(port, function() {
     console.log("Server listen on port", port);
